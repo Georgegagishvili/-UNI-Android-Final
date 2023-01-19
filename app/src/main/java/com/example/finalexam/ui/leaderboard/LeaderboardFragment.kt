fc8771cbf792ea664fc0f50b32460fc8d1411ea7
otomamatsashvili <otar.mamatsashvili@gmail.com>
2023-01-19 10:13:08 +0400

    feat: add menu button functionality

--- app/src/main/java/com/example/finalexam/ui/leaderboard/LeaderboardFragment.kt
+++ app/src/main/java/com/example/finalexam/ui/leaderboard/LeaderboardFragment.kt
@@ -6,9 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.finalexam.R
+import com.example.finalexam.databinding.FragmentGameBinding
+import com.example.finalexam.databinding.FragmentLeaderboardBinding
+import com.example.finalexam.ui.level_selection.LevelSelectionFragmentDirections
 
 class LeaderboardFragment : Fragment() {
+    private lateinit var binding: FragmentLeaderboardBinding
 
     companion object {
         fun newInstance() = LeaderboardFragment()
@@ -20,13 +25,22 @@ class LeaderboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false)
+        binding = FragmentLeaderboardBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(LeaderboardViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.menuButton.setOnClickListener {
+            val action = LeaderboardFragmentDirections.actionLeaderboardFragmentToMenuFragment()
+            view.findNavController().navigate(action)
+        }
     }
 
 }
